2cba8d25682322fbc06ec811f47d6679a08a4f70
Wotjala <marcin.wotjala@bottomline.com>
2022-06-10 23:00:45 +0100

    sitemaps

--- sitemap.config.js
+++ sitemap.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
-const config = {
+module.exports = {
   siteUrl: process.env.SITE_URL || 'https://latinshine.co.uk',
   generateRobotsTxt: true, // (optional)
-  exclude: ['/register', '/admin', '/api/insta', '/api/auth'],
+  exclude: ['/register', '/admin', '/api/insta', '/api/auth', '/account/profile', '/admin/customers', '/confirmation'],
   // ...other options
 }
-
-export default config
