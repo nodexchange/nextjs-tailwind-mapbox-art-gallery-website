5d566fda2e20cbdf4a67c0d124c219f62cd40753
Marcin Wojtala <marcinwojtala@Marcins-MBP.broadband>
2023-02-04 11:39:56 +0000

    salsa la content shift

--- next.config.js
+++ next.config.js
@@ -2,6 +2,20 @@ module.exports = {
 	reactStrictMode: true,
 	images: {
     domains: ['scontent.cdninstagram.com', 'cdninstagram.com'],
-  }
+  },
+	async redirects() {
+    return [
+      {
+        source: '/course-info',
+        destination: '/classes',
+        permanent: true,
+      },
+			{
+        source: '/courses',
+        destination: '/classes',
+        permanent: true,
+      }
+    ]
+  },
 	// optimizeFonts: false,
 };
