70ffce1d2494fc59c80ad2e3a13685500b42e100
Mars <nodexchange@outlook.com>
2023-10-31 08:51:42 +0000

    codemod

--- components/Article.js
+++ components/Article.js
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+import Image from "next/legacy/image";
 import Link from 'next/link';
 
 export const Article = ({ img, title, date, author, text, category, href, short, standalone }) => {
--- components/UpcomingEvent.js
+++ components/UpcomingEvent.js
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+import Image from "next/legacy/image";
 // import WishIcon from './WishIcon';
 
 const UpcomingEvent = ({ type, text, amount, children }) => {
